abd005d5a4aa47010cc28ac636712150332a496f
dovotori <web-asst@iMac-de-web-asst-2.local>
2014-01-07 15:05:29 +0100

    clean du code

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 - caméra isometrique ou projection orthogonale OK
 - texture de la carte projetée sur le relief OK
+
+
+- attendre que la texture d3.js soit chargée
 - centroid basé sur la position des capitales
 - fonction de transition pour la caméra et pour le mouvements des points
 - changement d'année
--- main.js
+++ main.js
@@ -3,6 +3,10 @@ window.addEventListener("load", setup, false);
 
 
 
+var largeurFenetre = window.innerWidth;
+var width = largeurFenetre;
+var height = window.innerHeight;
+
 
 
 var canvas;
@@ -12,12 +16,9 @@ var projection = d3.geo
 	.mercator();
 	//.conicEquidistant();
 	//.orthographic();
+projection.scale(80);
 var path = d3.geo.path().projection(projection);
 	
-var largeurFenetre = window.innerWidth;
-
-var width = largeurFenetre;
-var height = window.innerHeight;
 var imageTexture;
 
 
@@ -37,17 +38,28 @@ function setup()
 
 
 
+
+function lireCsv(url, callback) {
+	d3.csv(url, function(d){ callback(null, d); });
+}
+function lireJson(url, callback) {
+	d3.json(url, function(d){ callback(null, d); });
+}
+
+
+
+
+
+
+
 function ready(error, results) 
 {
-
-
 
 	// dessin de la carte avec d3
 	var svg = d3.select("#conteneur").append("svg")
 		.attr("width", width)
 	    .attr("height", height)
 	    .attr("id","svg");
-
 
 	    
 	var carted3js = svg.attr("id", "carted3js");
@@ -82,11 +94,9 @@ function ready(error, results)
 	context.putImageData( imageTextureData, 0, 0 );
 	//document.body.appendChild(canvas2d);
 
+	// creation de la texture THREE
 	var textureCarted3js = new THREE.Texture( canvas2d );
 	textureCarted3js.needsUpdate = true;
-
-
-
 
 
 	// dessin 3d
@@ -107,51 +117,12 @@ function ready(error, results)
 
 function animate()
 {
-
 	requestAnimationFrame( animate );	
 	canvas.draw();
 }
 
 
 
-
-
-
-
-
-//////////////////////////////////////////////
-///////////////// D3 ////////////////////////
-////////////////////////////////////////////
-
-function lireCsv(url, callback) {
-	d3.csv(url, function(d){ callback(null, d); });
-}
-function lireJson(url, callback) {
-	d3.json(url, function(d){ callback(null, d); });
-}
-
-
-
-function getPath(path){
-
-	var chaine = path.replace(/Z/g,""); 
-	chaine = chaine.replace(/[L,]/g," ");
-	var pathFragment = chaine.split("M");
-	var coor = [];
-
-
-	for(var i = 1; i < pathFragment.length; i++) // on commence a un car le split fait une premiere partie vide
-	{
-		var coordonnees = pathFragment[i].split(" ");
-		coor[i-1] = [];
-
-		for(var j = 0; j < coordonnees.length; j++) 
-		{
-			coor[i-1][j] = parseFloat(coordonnees[j]);
-		}	
-	}
-	return coor;
-}
 
 
 
@@ -167,10 +138,6 @@ function getPath(path){
 
 var Dessin = function()
 {
-
-	this.material;
-	this.material2;
-	this.material3;
 	this.materialMesh;
 	this.centroid;
 
@@ -179,6 +146,9 @@ var Dessin = function()
 
 	this.setup = function(scene, textureCarted3js)
 	{
+
+		console.log(textureCarted3js);
+
 		this.centroid = [];
 
 		var HG = projection([-180,84]);
@@ -188,10 +158,7 @@ var Dessin = function()
 		var MG = projection([-180,0]);
 		var MD = projection([180,0]);
 
-		// TEXTURE
-		var texture = THREE.ImageUtils.loadTexture("imageCarte.png");
-		// texture.wrapS = THREE.RepeatWrapping; 
-		// texture.wrapT = THREE.RepeatWrapping;
+		
 
 		//MATERIAL
 		this.materialMesh = new THREE.MeshLambertMaterial({ 
@@ -206,21 +173,8 @@ var Dessin = function()
 		this.centroid.push( [ BG[0] , BG[1] , 0 ]);
 		this.centroid.push( [ MG[0] , MG[1] , 0 ]);
 		this.centroid.push( [ MD[0] , MD[1] , 0 ]);
-
-		this.material = new THREE.LineBasicMaterial({
-	        color: 0x666666,
-			linewidth: 1
-	    });
-	    this.material2 = new THREE.LineBasicMaterial({
-	        color: 0xffffff,
-			transparent: true, opacity: 0
-	    });
-	    this.material3 = new THREE.LineBasicMaterial({
-	        color: 0x6666ff,
-			transparent: true, opacity: 1
-	    });	
 		
-      	
+
 	}
 
 
@@ -228,117 +182,14 @@ var Dessin = function()
 
 	this.draw = function(scene, data, dataIndex)
 	{		
-
-		
-		this.dessinerCartePlate(scene, data, dataIndex);
+	
 		this.dessinerNotesPays(scene);
-
 
 	}
 	
 
 
-	this.dessinerCartePlate = function(scene, data, dataIndex)
-	{
-		// dessin de la carte
-		for(var k = 0; k < data.features.length; k++)
-		{
-
-			// couleur du pays
-			var isIndexed = false;
-			var color = "#333";
-			for(var l = 0; l < dataIndex.length; l++)
-			{
-
-
-				if(dataIndex[l].iso == data.features[k].id)
-				{
-					
-					isIndexed = true;
-
-					switch(dataIndex[l].cat)
-					{
-						case "Very serious situation": 	color = "#000000"; break;
-						case "Difficult situation": 	color = "#dc002e"; break;
-						case "Noticeable problems": 	color = "#f29901"; break;
-						case "Satisfactory situation": 	color = "#fee504"; break;
-						case "Good situation": 			color = "#fff"; break;
-						default: 						color = "#000"; break;
-					}
-				}	
-			}
-
-
-
-			if(isIndexed)
-			{
-
-
-				// calcul du centroid
-				var centroidTemporaire = path.centroid(data.features[k]);
-				var centro = [centroidTemporaire[0], centroidTemporaire[1], -(k)*0.2 ]			
-				this.centroid.push( centro );
-
-				/*
-				// traits du centroid vers la note
-				var g = new THREE.Geometry();
-				g.vertices.push(new THREE.Vector3(centro[0], centro[1], 0));
-				g.vertices.push(new THREE.Vector3(centro[0], centro[1], centro[2]));
-				var l = new THREE.Line(g, this.material3);
-				scene.add(l);
-				*/
-
-			}
-
-			//this.dessinerFormePays(scene, data, color, k);
-
-		}
-	}
-
-
-
-
-	this.dessinerFormePays = function(scene, data, color, k)
-	{
-
-		var material = new THREE.MeshBasicMaterial( { color: color, transparent: true, opacity: 0.8 } );
-		material.side = THREE.DoubleSide;
-
-		// recuperer les coordonnees
-		var coor = getPath(path(data.features[k]));
-
-		for(var i = 0; i < coor.length; i++)
-		{
-			var c = [];
-			var geometryBorder = new THREE.Geometry();
-
-			for(var j = 0; j < coor[i].length; j+=2)
-			{
-				c.push( new THREE.Vector2 ( coor[i][j], coor[i][j+1] ) );
-				geometryBorder.vertices.push(new THREE.Vector3(coor[i][j], coor[i][j+1], 0));
-			}
-			
-			// dernier points pour fermer la forme
-			c.push( new THREE.Vector2(coor[i][0], coor[i][1] ) );
-			geometryBorder.vertices.push(new THREE.Vector3(coor[i][0], coor[i][1], 0));
-
-			// creation de la forme et de sa geometrie
-			var shape = new THREE.Shape(c);
-			var geometry = new THREE.ShapeGeometry( shape );
-
-			// dessin de la forme pleine
-			var mesh = new THREE.Mesh( geometry, material );
-			scene.add(mesh);
-
-			// dessin de la ligne
-			var line = new THREE.Line(geometryBorder, this.material);
-			line.position.set(0, 0, -1);
-			
-			scene.add(line);
-
-		}
-		
-	}
+	
 
 
 
@@ -358,9 +209,9 @@ var Dessin = function()
 			geometrie.faces.push( new THREE.Face3( 3*i, 1+3*i, 2+3*i ));
 
 		    geometrie.faceVertexUvs[0].push([
-		        new THREE.Vector2( map(delaunay[i][0][0],1,width,0,1), map(delaunay[i][0][1],1,height,1,0) ),
-		        new THREE.Vector2( map(delaunay[i][1][0],1,width,0,1), map(delaunay[i][1][1],1,height,1,0) ),
-		        new THREE.Vector2( map(delaunay[i][2][0],1,width,0,1), map(delaunay[i][2][1],1,height,1,0) )
+		        new THREE.Vector2( map(delaunay[i][0][0], 0,width, 0,1), map(delaunay[i][0][1], 0,height, 1,0) ),
+		        new THREE.Vector2( map(delaunay[i][1][0], 0,width, 0,1), map(delaunay[i][1][1], 0,height, 1,0) ),
+		        new THREE.Vector2( map(delaunay[i][2][0], 0,width, 0,1), map(delaunay[i][2][1], 0,height, 1,0) )
 	        ]);
 
 	    }
@@ -436,7 +287,7 @@ var Canvas = function()
 
 		// LIGHT
 		this.scene.add( new THREE.AmbientLight( 0x212223 ) );
-		var light = new THREE.DirectionalLight( 0xffffff, 1 );
+		var light = new THREE.DirectionalLight( 0xffffff, 0.1 );
 		light.position.set( -200, 0, -200 );
 		this.scene.add(light);
 
